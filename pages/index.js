0d596908c77a38728d4df82fb897f794928c31df
Alessio Franceschi <alessiofranceschi2@gmail.com>
2020-12-08 18:28:26 +0100

    testing out dynamic imports

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,12 @@
 import Head from "next/head";
 
-import { getAllPosts } from "../lib/sanityClient";
-
-import Navbar from "../components/Navbar";
-import PostHomepage from "../components/PostHomepage";
 import { getPosts } from "../lib/cache";
 
 import { Fragment } from "react";
 
 export default function Home(props) {
+	const PostHomepage = dynamic(() => import("../components/PostHomepage"));
+
 	const homepagePosts = props.posts.map(post => {
 		return <PostHomepage post={post} key={post._id} />;
 	});
@@ -31,12 +29,7 @@ export default function Home(props) {
 }
 
 export async function getStaticProps() {
-	// const posts = await getAllPosts();
-	// return {
-	// 	props: { posts: posts },
-	// };
 	const posts = await getPosts();
-	const again = await getPosts();
 	return {
 		props: { posts: posts },
 	};
