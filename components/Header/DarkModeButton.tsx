510f3eb5e1c4f9b89faa766421be8f6f40676c7c
Alessio Franceschi <alessiofranceschi2@gmail.com>
2021-04-24 19:22:20 +0200

    Sun/Moon dark mode button

--- components/Header/DarkModeButton.tsx
+++ components/Header/DarkModeButton.tsx
@@ -1,10 +1,16 @@
+import SunMoon from "./SunMoonSVG";
+
 export default function DarkModeButton() {
 	let isDark = true;
 
 	if (typeof window !== "undefined") {
 		var htmlTag = document.querySelector("html")!
+		var overlay = document.querySelector("#overlay")!
 		isDark = localStorage.getItem("theme") === "dark" ? true : false;
-		if (isDark) htmlTag.classList.add("dark");
+		if (isDark) {
+			htmlTag.classList.add("dark");
+			overlay.classList.add("mooned");
+		}
 		document.documentElement.style.setProperty(
 			"--hamb-color",
 			isDark ? "#FFFFFF" : "#3a3a3a"
@@ -18,6 +24,7 @@ export default function DarkModeButton() {
 	function toggleDarkMode() {
 		isDark = !isDark;
 		htmlTag.classList.toggle("dark");
+		overlay.classList.toggle("mooned");
 		document.documentElement.style.setProperty(
 			"--hamb-color",
 			isDark ? "#FFFFFF" : "#3a3a3a"
@@ -27,7 +34,9 @@ export default function DarkModeButton() {
 
 	return (
 		<div onClick={toggleDarkMode} className="switch">
-			<div className="ball" aria-label="Switch per Dark Mode"></div>
+			<div className="ball" aria-label="Switch per Dark Mode">
+				<SunMoon />
+			</div>
 		</div>
 	);
 }
