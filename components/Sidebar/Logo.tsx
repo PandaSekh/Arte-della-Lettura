692167bd0360432316a15ff0a34a17c18c7e09f7
Alessio <alessiofranceschi2@gmail.com>
2021-06-17 16:39:23 +0200

    test

--- components/Sidebar/Logo.tsx
+++ components/Sidebar/Logo.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 export default function Logo(): JSX.Element {
   return (
-    <div className="">
+    <motion.div drag>
       <Image width={200} height={200} src="/apple-icon-180x180-dunplab-manifest-37208.png" />
-    </div>
+    </motion.div>
   );
 }
