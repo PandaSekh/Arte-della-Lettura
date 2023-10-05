7d0a82d561cd5f832de0e6c0c5de9837bba0fbb7
Alessio Franceschi <alessiofranceschi2@gmail.com>
2023-10-05 22:43:22 +0200

    added Meticulous

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,19 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="it">
-      <Head />
+      <Head>
+        {(process.env.NODE_ENV === "development" ||
+          process.env.VERCEL_ENV === "preview") && (
+          // eslint-disable-next-line @next/next/no-sync-scripts
+          <script
+            data-project-id="wZJ8LP6YhJJUjAajtqRD4iEUgrwR3TGKt2g0bSpe"
+            data-is-production-environment="false"
+            src="https://snippet.meticulous.ai/v1/meticulous.js"
+          />
+        )}
+      </Head>
       <body>
         <Main />
         <NextScript />
