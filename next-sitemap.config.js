bf3c95663f24a875d475a8d4f45403ec5bc5949a
Alessio Franceschi <alessiofranceschi2@gmail.com>
2023-10-05 23:09:41 +0200

    update sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,7 @@
+/** @type {import("next-sitemap").IConfig} */
 module.exports = {
   siteUrl: process.env.SITE_URL || "https://www.artedellalettura.it",
   generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  sitemapSize: 7000,
 };
